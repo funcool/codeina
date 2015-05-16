51f335351598e0f57742a89bf278137aa931b61c
Andrey Antukh <niwi@niwi.nz>
2015-05-16 14:21:07 +0200

    Set version to 0.1.0

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject funcool/codeina "0.1.0-SNAPSHOT"
+(defproject funcool/codeina "0.1.0"
   :description "Generate documentation from Clojure source files"
   :url "https://github.com/funcool/codeina"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
-
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [org.clojure/clojurescript "0.0-3269"]
                  [org.clojure/tools.namespace "0.2.10"]
-                 [org.clojure/clojurescript "0.0-3126"]
                  [leinjacker "0.4.2"]
                  [hiccup "1.0.5"]
                  [org.pegdown/pegdown "1.4.2"]]
